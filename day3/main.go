122de53c1e52e3d1cbabeb0bb13e60b803a317cc
rkabani19 <rohail.kabani@gmail.com>
2020-12-09 11:59:59 -0500

    day 3

---
+++ day3/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	var data []string // should be of type byte but I'm too lazy
+	readFile(&data)
+
+	// part 1
+	a := printNumTrees(data, 3, 1)
+	fmt.Printf("Part 1: %d\n", a)
+
+	// part 2
+	b := printNumTrees(data, 1, 1)
+	c := printNumTrees(data, 5, 1)
+	d := printNumTrees(data, 7, 1)
+	e := printNumTrees(data, 1, 2)
+	fmt.Printf("Part 2: %d\n", a*b*c*d*e)
+}
+
+func printNumTrees(data []string, right int, down int) int {
+	col := 0
+	numTrees := 0
+
+	for i := 0; i < len(data); i += down {
+		row := data[i]
+		if len(row) <= col {
+			col %= len(row)
+		}
+
+		cur := string(row[col])
+		if cur == "#" {
+			numTrees++
+		}
+
+		col += right
+	}
+
+	return numTrees
+}
+
+func readFile(data *[]string) {
+	file, err := os.Open("day3.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		i := scanner.Text()
+		*data = append(*data, i)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
