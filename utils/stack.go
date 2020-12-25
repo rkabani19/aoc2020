82e9315519d8daa6d54d2ef0236839396380acd6
rkabani19 <rohail.kabani@gmail.com>
2020-12-25 15:37:43 -0500

    add stack implementation

---
+++ utils/stack.go
@@ -0,0 +1,29 @@
+package utils
+
+type Stack []string
+
+func (s *Stack) IsEmpty() bool {
+	return len(*s) == 0
+}
+
+func (s *Stack) Push(str string) {
+	*s = append(*s, str)
+}
+
+func (s *Stack) Pop() (string, bool) {
+	if s.IsEmpty() {
+		return "", false
+	} else {
+		index := len(*s) - 1
+		element := (*s)[index]
+		*s = (*s)[:index]
+		return element, true
+	}
+}
+
+func (s *Stack) Peek() string {
+	if s.IsEmpty() {
+		return ""
+	}
+	return (*s)[len(*s)-1]
+}
